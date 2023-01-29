5983b7a806d55b64b668d155de1c3683557ad04a
einsundnull <35996151+einsundnull@users.noreply.github.com>
2023-01-29 12:18:08 +0400

    Third Commit

--- GitTest/src/app/StartTest.java
+++ GitTest/src/app/StartTest.java
@@ -5,6 +5,7 @@ public class StartTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test Successful");
+		System.out.println("Second");
 	}
 
 }
